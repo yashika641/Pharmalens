91c683d5a89a9dab68b3655ec487369429b42f23
yashika641 <123154422+yashika641@users.noreply.github.com>
2026-01-27 16:41:36 +0530

    6

--- backend/main.py
+++ backend/main.py
@@ -1,48 +1,7 @@
-from dotenv import load_dotenv
-import os
-
-# Load env FIRST, before any other imports
-load_dotenv("C:\\Users\\palya\\Desktop\\pharmalens\\Pharmalens\\backend\\.env")
-import os
-print("ENV CHECK:", os.getenv("SUPABASE_URL"))
-print("ENV CHECK:", os.getenv("SUPABASE_ANON_KEY"))
-import fastapi
-from backend.routes import login_routes 
-from backend.routes import scan_routes 
-from backend.routes import user_profile_routes
-from backend.routes import drug_routes
-from backend.routes import chatbot_routes
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import Request, Response
 from fastapi import FastAPI
 
 app = FastAPI()
 
-app.include_router(login_routes.router)
-app.include_router(scan_routes.router)
-app.include_router(user_profile_routes.router)
-app.include_router(drug_routes.router)
-app.include_router(chatbot_routes.router)
-origins = [
-    "http://localhost:3000",
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "https://pharmalenss.netlify.app",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    response: Response = await call_next(request)
-    return response
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+def health():
+    return {"status": "ok"}
