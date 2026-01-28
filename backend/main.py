75038e8950801e7aff8e0b69fcc4089fb3b01de2
yashika641 <123154422+yashika641@users.noreply.github.com>
2026-01-29 01:29:12 +0530

    deployement changes

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
-from dotenv import load_dotenv
 import os
 
+PORT = int(os.getenv("PORT", 8000))
+from dotenv import load_dotenv
+import os
+import os
+from qdrant_client import QdrantClient
+from sentence_transformers import SentenceTransformer
 # Load env FIRST, before any other imports
 load_dotenv("C:\\Users\\palya\\Desktop\\pharmalens\\Pharmalens\\backend\\.env")
 import os
@@ -11,18 +16,46 @@ from backend.routes import login_routes
 from backend.routes import scan_routes 
 from backend.routes import user_profile_routes
 from backend.routes import drug_routes
-from backend.routes import chatbot_routes
+# from backend.routes import chatbot_routes
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import Request, Response
 from fastapi import FastAPI
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("🚀 FastAPI app starting...")
 
 app = FastAPI()
+
+qdrant = None
+embedding_model = None
+
+
+@app.on_event("startup")
+def startup():
+    global qdrant, embedding_model
+
+    QDRANT_URL = os.getenv("QDRANT_URL")
+    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+
+    if not QDRANT_URL or not QDRANT_API_KEY:
+        raise RuntimeError("Qdrant env vars missing")
+
+    qdrant = QdrantClient(
+        url=QDRANT_URL,
+        api_key=QDRANT_API_KEY,
+        prefer_grpc=False,
+        timeout=30,
+    )
+
+    embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    print("✅ Qdrant & embedding model initialized")
 
 app.include_router(login_routes.router)
 app.include_router(scan_routes.router)
 app.include_router(user_profile_routes.router)
 app.include_router(drug_routes.router)
-app.include_router(chatbot_routes.router)
+# app.include_router(chatbot_routes.router)
 origins = [
     "http://localhost:3000",
     "http://localhost:3000",
